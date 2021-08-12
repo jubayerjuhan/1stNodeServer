8731bf695af2a4ce860ee85728799807f58f7d70
jubayerjuhan <davidjuhan23@gmail.com>
2021-08-12 22:53:50 +0600

    url params

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const express = require('express')
 const app = express();
 
+const demoFunc = (req, res) => {
+    const user_Info = {
+        name: 'Juhan',
+        age: 20,
+        surename: 'Jubayer'
+    }
+
+    res.send(user_Info)
+}
+
+const users = ['juhan', 'tazin', 'tuba', 'junayet', 'tasfia', 'sara', 'dip']
 
 
-app.get('/',(req, res) => {
-    res.send('Hello from the root path')
-})
-app.get('/juhan', (req, res) => {
-    res.send('Hello from the Juhan path')
-})
-app.get('/jubayer', (req, res) => {
-    res.send('Hello from the Jubayer path')
-})
-app.listen(8000)
+app.listen(3000)
+app.get('/juhan', demoFunc)
 
+app.get('/users/:id', (req, res) => {
+    const userId = req.params.id;
+    const user = users[userId];
+    res.send(user)
+})
